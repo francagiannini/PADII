61465ed62864a4bef0d96f1d73332fb9e56f3425
Franca Giannini-Kurina <au710823@uni.au.dk>
2023-11-03 19:22:37 +0100

    bbaby

--- babysteps.R
+++ babysteps.R
@@ -1,18 +1,20 @@
+library(tidyverse)
+theme_set(theme_bw())
+theme_update(panel.grid = element_blank())
+
+# Instalamos INLA ----
+
 # install.packages(
 #   "INLA",
-#   repos = c(getOption("repos"), INLA = "https://inla.r-inla-download.org/R/stable"),
+#   repos = c(getOption("repos"), 
+#   INLA = "https://inla.r-inla-download.org/R/stable"),
 #   dep = TRUE
 # )
-# 
+
 # install.packages("BiocManager")
 # BiocManager::install(c("graph", "Rgraphviz"), dep=TRUE)
-#https://mcmc-jags.sourceforge.io/
-install.packages("rjags")
-
 
 library(INLA)
-Sys.setenv(JAGS_HOME="C:/Program Files/JAGS/JAGS-4.3.1")
-library(rjags)
 
 # distribuciones o modelos que GMRF
 
@@ -22,100 +24,80 @@ names(inla.models()$likelihood)
 
 inla.doc()
 
-#Simulamos rapidito 
+# Simulamos rapidito ----
 
-N <- 100 # 500, 5000, 25000, 100000
+N <- 500
 x <- rnorm(N, mean = 6, sd = 2)
 y <- rnorm(N, mean = x, sd = 1)
-data <- list(x = x, y = y, N = N)
+data <- as.data.frame(bind_cols(x = x, y = y))
 
-model <- function() {
-  for(i in 1:N) {
-    y[i] ~ dnorm(mu[i], tau)
-    mu[i] <- alpha + beta * x[i]
-  }
-  alpha ~ dnorm(0, 0.001)
-  beta  ~ dnorm(0, 0.001)
-  tau   ~ dgamma(0.01, 0.01)
-}
+# Ajuste con lm
 
-# Ajuste con JAGS
+gau_lm <- lm(y ~ x,
+             data = data)
 
-params <- c("alpha", "beta", "tau", "mu")
-
-rjags::jags.model(
-  data = data,
-  param = params,
-  n.chains = 3,
-  n.iter = 50000,
-  n.burnin = 5000,
-  model.file = model
-)
-
+summary(gau_lm)
 
 # Ajuste con INLA
 
 gau <- inla(y ~ x,
      family = "gaussian",
-     data = data,
-     control.predictor = list(link = 1)
+     data = data#,
+     #control.predictor = list(link = 1)
 )
 
 
 # Exploramos un output
 summary(gau)
 
-# Simulamos rapidito para un conteo
+plot(gau)
 
-N <- 100 # 500, 5000, 25000, 100000
-x <- rnorm(N, mean = 5, sd = 1)
-nu <- rnorm(N, 0, 0.1)
-mu <- exp(1 + 0.5 * x + nu)
-y <- rpois(N, mu)
+# Gómez-Rubio, Virgilio (2020). 
+# Bayesian Inference with INLA. Chapman & Hall/CRC Press. Boca Raton, FL.
+# https://becarioprecario.bitbucket.io/inla-gitbook/index.html
+
+library("MASS")
+summary(cement)
+
+GGally::ggpairs(cement)
+
+m1_lm <- lm(y ~ x1 + x2 + x3 + x4, data = cement)
+summary(m1_lm)
+
+m1 <- inla(y ~ x1 + x2 + x3 + x4, data = cement
+           ,control.compute = list(config = TRUE))
+
+summary(m1)
+
+m1$cpo
+
+m1.samp <- inla.posterior.sample(100, m1)
+
+names(m1.samp[[1]])
+
+m1.samp[[1]]
+
+x1x2.samp <- inla.posterior.sample.eval(function(...) {x1 * x2},
+                                        m1.samp)
+
+summary(as.vector(x1x2.samp))
+
+prec.samp <- inla.hyperpar.sample(1000, m1)
+
+tab <- data.frame(prec = prec.samp[, 1])
+
+ggplot(tab, aes(x = prec)) +
+  geom_histogram(aes(y=..density..), bins = 18) +
+  geom_line(data = as.data.frame(m1$marginals.hyperpar[[1]]),
+            aes(x = x, y = y)) + 
+  xlab(expression(tau)) +
+  ylab(expression(paste(pi, "(", tau, " | ", bold(y), ")"))) +
+  xlim(0, 0.8)
 
 
-# Con JAGS
 
-model_p <-  function() {
-  for(i in 1:N) {
-    y[i] ~ dpois(mu[i])
-    log(mu[i]) <- alpha + beta * x[i] + nu[i]
-    nu[i] ~ dnorm(0, tau.nu)
-  }
-  alpha  ~ dnorm(0, 0.001)
-  beta   ~ dnorm(0, 0.001)
-  tau.nu ~ dgamma(0.01, 0.01)
-}
 
-params model =  function() {
-  for(i in 1:N) {
-    y[i] ~ dpois(mu[i])
-    log(mu[i]) <- alpha + beta*x[i] + nu[i]
-    nu[i] ~ dnorm(0, tau.nu)
-  }
-  alpha  ~ dnorm(0, 0.001)
-  beta   ~ dnorm(0, 0.001)
-  tau.nu ~ dgamma(0.01, 0.01)
-}
-params <- c("alpha", "beta", "tau.nu", "mu")
 
-jags(
-  data = data,
-  param = params,
-  n.chains = 3,
-  n.iter = 50000,
-  n.burnin = 5000,
-  model.file = model
-)
 
-# Con INLA
-
-nu <- 1:N
-
-poi <- inla(y ~ x + f(nu, model = "iid"),
-     family = "poisson",
-     data = data,
-     control.predictor = list(link = 1)
-)
 
 
